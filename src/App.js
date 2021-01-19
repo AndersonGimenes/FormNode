a2c017a457302ac53f122c32856488cedbd15ce2
Anderson Dev <gcosta19@gmail.com>
2021-01-18 22:41:38 -0300

    Terceiro modulo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+import { Container, Typography } from '@material-ui/core';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import './assets/App.css';
+import 'fontsource-roboto';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center">
+          Formulário Cadastro
+      </Typography>
+        <FormularioCadastro enviar={enviar} validarCpf={validarCpf}/>
+      </Container>
+    );
+  }
+}
+
+function enviar(dados) {
+  console.log(dados);
+}
+
+function validarCpf(cpf){
+  if(cpf.length !== 11){
+    return {valido: false, texto: 'O CPF deve ter 11 digitos'}
+  }
+
+  return {valido: true, texto: ''}
 }
 
 export default App;
